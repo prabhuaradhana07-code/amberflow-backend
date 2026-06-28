72541e8833e859f3a0314c164f125a443d5205b8
unknown <prabhuaradhana07@gmail.com>
2026-06-28 23:58:51 +0530

    chore: add security headers to next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self';",
+          }
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
